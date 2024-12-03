e0336f5daa914f3df8c86046a24ec8348b07763a
AgnetheCFlorentsen <agfl0001@stud.kea.dk>
2024-12-03 11:15:27 +0100

    added styling to accountpage and change password

--- js/myAccount.js
+++ js/myAccount.js
@@ -1,6 +1,7 @@
-import { parseJwt  } from "./admin.js";
+import {parseJwt} from "./admin.js";
+
 export function loadmyAccount() {
-    const token =localStorage.getItem('user');
+    const token = localStorage.getItem('user');
     const string = parseJwt(token)
     console.log(string)
     const email = string["sub"]
@@ -21,7 +22,13 @@ export function loadmyAccount() {
 
         // Create the form dynamically
         app.innerHTML = `
-        <h3>Edit User Information</h3>
+<link rel="stylesheet" href="css/myAccount.css">
+<br>
+<br>
+<br>
+<div class="container">
+    <!-- Edit User Information -->
+    <div class="form-section"><h3>Edit User Information</h3>
         <form id="user-edit-form">
             <label for="firstName">First Name:</label>
             <input type="text" id="firstName" name="firstName" value="${user.firstName}" />
@@ -34,17 +41,27 @@ export function loadmyAccount() {
 
             <button type="button" id="save-button">Save Changes</button>
         </form>
+        <br>
         <p id="form-message"></p>
-        
+    </div>
+
+    <!-- Edit Your Password -->
+    <div class="form-section">
          <h3>Edit Your Password</h3>
         <form id="password-edit-form">
       
-            <label for="password">Password:</label>
+            <label for="password">New password:</label>
             <input type="password" id="password" name="password" value="12345"/>
+            
+            <label for="password">Confirm new password:</label>
+            <input type="password" id="password-confirmation" name="password-confirmation" value="12345"/>
 
             <button type="button" id="save-button-password">Save Changes</button>
         </form>
+        <br>
         <p id="form-message-password"></p>
+   </div>
+</div>
     `;
 
 
@@ -81,28 +98,36 @@ export function loadmyAccount() {
                 });
         });
 
-            saveButtonPassword.addEventListener('click', () => {
+        saveButtonPassword.addEventListener('click', () => {
+            const password = document.getElementById('password').value
+            const passwordConfirmation = document.getElementById('password-confirmation').value
+            if (password === passwordConfirmation) {
                 const updatedUser = {
-                    password: document.getElementById('password').value,
+                    password: password,
                 };
-            fetch(`http://localhost:8080/api/v1/users/${id}/updatepassword`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(updatedUser),
-            })
-                .then(response => {
-                    if (response.ok) {
-                        formMessagePassword.textContent = 'Password updated successfully!';
-                    } else {
-                        throw new Error('Failed to update user.');
-                    }
+                fetch(`http://localhost:8080/api/v1/users/${id}/updatepassword`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(updatedUser),
                 })
-                .catch(error => {
-                    console.error('Error updating user:', error);
-                    formMessage.textContent = 'An error occurred while updating the user.';
-                });
-        });
-}}
+                    .then(response => {
+                        if (response.ok) {
+                            formMessagePassword.textContent = 'Password updated successfully!';
+                        } else {
+                            throw new Error('Failed to update password.');
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error updating user:', error);
+                        formMessagePassword.textContent = 'An error occurred while updating the password.';
+                    });
+            } else {
+                formMessagePassword.textContent = 'Passwords not matching';
+            }
+        })
+
+    }
+}
 
