c2e0be5f252794b91b89e9a945e78dd9ba0ffd5a
Gerberg1 <144436875+Gerberg1@users.noreply.github.com>
2024-12-10 12:37:37 +0100

    stock buttons only if admin

--- js/products/products.js
+++ js/products/products.js
@@ -109,10 +109,12 @@ function createProductsHTML(products, currentPage, totalPages, sortOrder, lowSto
         <div class="container">
             <div class="d-flex justify-content-between align-items-center mb-3">
                 <div>
-                    <input type="checkbox" id="lowStockFilter" class="form-check-input" ${lowStock ? 'checked' : ''}>
+                ${checkAdmin() ? `
+                            <input type="checkbox" id="lowStockFilter" class="form-check-input" ${lowStock ? 'checked' : ''}>
                     <label for="lowStockFilter" class="form-check-label">Low Stock</label>
                     <input type="checkbox" id="outOfStockFilter" class="form-check-input ms-3" ${outOfStock ? 'checked' : ''}>
                     <label for="outOfStockFilter" class="form-check-label">Out of Stock</label>
+                   ` : ''}
                 </div>
 
                 <div>
